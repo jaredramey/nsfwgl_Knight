f634577434804806237490979912422d6cd09116
Jeffrey Johnson <jefjohms@gmail.com>
2015-10-23 13:51:28 -0700

    fixed cpass

--- defer/CPass.h
+++ defer/CPass.h
@@ -20,7 +20,8 @@ public:
 	{ 
 		//TODO_D("glUseProgram, glClear, glBindFrameBuffer, glViewPort, glEnable etc..."); 
 		glClearColor(1.f, .5f, 1.f, 1);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);//being thorough
+		glClear(GL_COLOR_BUFFER_BIT);//no depth
 		glUseProgram(*shader);
 	}
 
@@ -38,7 +39,7 @@ public:
 		//setUniform("Position",	nsfw::UNIFORM::TEX2, position, 1);
 		//setUniform("Normal",	nsfw::UNIFORM::TEX2, normal,   2);
 		//setUniform("Depth",		nsfw::UNIFORM::TEX2, depth,    3);
-		setUniform("Light",		nsfw::UNIFORM::TEX2, light,    4);
+		setUniform("Light",		nsfw::UNIFORM::TEX2, light,    1);
 
 		//setUniform("TexelScalar", nsfw::UNIFORM::MAT4, glm::value_ptr(nsfw::Window::instance().getTexelAdjustmentMatrix()));
 
